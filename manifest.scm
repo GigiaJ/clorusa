61896b6ca4793344152acc5f5710cdbe901fb280
GigiaJ <gigiaboone@yahoo.com>
2026-02-17 15:35:47 -0600

    Add manifest file

---
+++ manifest.scm
@@ -0,0 +1,20 @@
+(specifications->manifest
+ '(
+   ;; Need 1.88 and we're simply... testing
+   ; "rust"
+   "curl"
+   "grep"
+   "gcc-toolchain"
+   "pkg-config"
+   "which"
+   "zlib"
+    "binaryen" ; This provides wasm-opt
+   "node"
+   "openssl"
+   "yarn"
+   "git"
+   "bash"
+   "sed"
+   "nss-certs"
+  ; "rust:cargo"
+    "coreutils"))
